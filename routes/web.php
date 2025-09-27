61319d14a362c451d645a7dde9c9ff07057f09b0
jplataquin <jp.lataquin@gmail.com>
2025-09-27 17:24:52 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,7 @@ Route::middleware('auth')->group(function () {
 //employee/photo/1758955450_1.
 Route::get('/employee/photo/{$photo}', function($photo){
 
+    
     $response = Response::make(File::get( storage_path('app/public/employee/photos/'.$photo) ), 200);
     $response->header("Content-Type", 'image/jpeg');
 
