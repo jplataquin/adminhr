f4ad4642f60bb134dc22a7a3d55b62cf7cc07304
jplataquin <jp.lataquin@gmail.com>
2025-10-01 13:17:24 +0800

    fix

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -691,7 +691,7 @@ class EmployeeController extends Controller
         
         ];
 
-        $division_options = $employees->division_option();
+        $division_options = $employees->division_options();
 
         return view('employee/print',[
             'divisions' => $divisions,
