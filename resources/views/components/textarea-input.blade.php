79e01f928bb376904f2c1981651dcdee6ea08f39
jplataquin <jp.lataquin@gmail.com>
2025-10-04 14:27:26 +0800

    fix

--- resources/views/components/textarea-input.blade.php
+++ resources/views/components/textarea-input.blade.php
@@ -4,11 +4,19 @@
     'id'            => '',
     'placeholder'   => '',
     'rows'          => '',
-    'name'          => ''
+    'name'          => '',
+    'required'      => false
 ])
 
 @if($label != '')
-<label class="text-sm font-medium dark:text-white block mb-2">{{$label}}</label>
+<label class="text-sm font-medium dark:text-white block mb-2">
+    {{$label}}
+
+    @if($required)
+        <span class="text-red-500 text-sm">*</span>
+    @endif
+
+</label>
 @endif
 
 <textarea @disabled($disabled) name="{{$name}}" placeholder="{{$placeholder}}" id="{{$id}}" rows="{{$rows}}" {{ $attributes->merge(['class' => "border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full p-2.5"]) }}>{{ $slot }}</textarea>
